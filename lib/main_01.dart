33fc8163c74a4bc5c8cd73d0ca1101a6679398c9
Gao <gaozhili01@baidu.com>
2021-01-31 22:40:27 +0800

    init the directory

---
+++ lib/main_01.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        appBar:AppBar(
+          title:Text('Flutter Demo')
+        ),
+        body:HomeContent(),
+      )
+    );
+  }
+}
+
+class HomeContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child:Text(
+          '你好，flutter!',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 40.0,
+            color: Colors.yellow,
+          ),
+        )
+    );
+  }
+
+}
